20eb7d6c4e7cf364c49487044c38a60d105c3bb5
Vegesna, Vijay Varma <vijayv.vegesna@iqvia.com>
2022-07-16 22:02:59 -0700

    Initial Commit

---
+++ AsyncAndAwaitPractice/AsyncAndAwaitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAndAwaitPracticeApp.swift
+//  AsyncAndAwaitPractice
+//
+//  Created by Vegesna, Vijay Varma on 7/16/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAndAwaitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncAndAwaitPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncAndAwaitPractice
+//
+//  Created by Vegesna, Vijay Varma on 7/16/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
